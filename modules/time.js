43ce25a73267d96f1825a1031c50b4edcf634e38
Piotr Wlazeł <pancu1222@gmail.com>
2019-07-30 02:49:25 +0200

    update time.js

--- modules/time.js
+++ modules/time.js
@@ -1,8 +1,8 @@
 function getTime(time) {
   var restOFTime = time;
-  var hours = Math.floor(time / 360);
+  var hours = Math.floor(time / 3600);
   //console.log(hours);
-  restOFTime = restOFTime - hours * 360;
+  restOFTime = restOFTime - hours * 3600;
   var mintues = Math.floor(restOFTime / 60);
   //console.log(mintues);
   restOFTime = restOFTime - mintues * 60;
